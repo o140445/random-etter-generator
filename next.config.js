12aec4fda73464e6d9fc198d016ecb70a6afba81
o140445 <o140445s@gmail.com>
2025-06-29 02:21:18 +0800

    e selint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App Router is now stable and enabled by default
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
