5656b53ba2bc8d59ce9a3bf6695ecba7716558e9
o140445 <o140445s@gmail.com>
2025-06-29 02:27:15 +0800

    e selint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   // App Router is now stable and enabled by default
 
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
--- src/components/RandomLetterGenerator.tsx
+++ src/components/RandomLetterGenerator.tsx
@@ -69,7 +69,6 @@ export default function RandomLetterGenerator() {
 
   useEffect(() => {
     generateRandomString();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
